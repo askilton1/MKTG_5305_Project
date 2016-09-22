6f9e8e518f8967075cface1755d04a06114f1757
askilton1 <antonio.skilton@gmail.com>
2016-09-22 16:38:56 -0700

    new first_time column

--- clean.r
+++ clean.r
@@ -1,3 +1,4 @@
+rm(ls=list())
 library(dplyr)
 print("reading data")
 data <- tbl_df(read.csv("data.csv"))
@@ -7,7 +8,9 @@ data <- data %>%
          dow = weekdays(date), 
          weekend = ifelse(dow=="Saturday" | dow=="Sunday",1,0),
          class = Classification,
-         turn1 = ifelse(Door.Name == "FITN-TRN1",1,0)) %>%
+         turn1 = ifelse(Door.Name == "FITN-TRN1",1,0),
+         first_time = ifelse(1:length(anonID) %in% match(unique(anonID),anonID),0,1)) %>%
   select(-Door.Access.Actual.DateTime,-Result,-Door.Name,-Classification) %>%
   filter(date >= "2013-06-15" & date <= "2014-06-15")
 print("done")
+
