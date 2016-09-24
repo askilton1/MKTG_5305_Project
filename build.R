f1a4d0e4a0ff0dca83ec3ffecd05a9ca1a6bccc7
askilton1 <antonio.skilton@gmail.com>
2016-09-24 13:35:33 -0700

    on the way to producing a plot and a table for all three groups

--- build.R
+++ build.R
@@ -1,23 +1,37 @@
 source("clean.r")
 library(dplyr)
-data2 <- data %>%
-  group_by(date) %>%
-  summarise(total_visitors = n(),unique_visitors = length(unique(anonID)),sum_new_visitors = sum(first_time))
-write.csv(data2,"by_day.csv") 
-attach(data2)
-plot(date,sum_new_visitors,main = "New Visitors by Day \nBetween 6/30/14 and 7/1/15",ylab = "New Visitors")
-detach(data2);rm(data2)
+library(ggplot2)
+library(reshape2)
 
-data %>%
+##----- total visitors, unique visitors, by day
+by_day <- data %>%
+  group_by(date) %>%
+  summarise(totalVisitors = n(),uniqueVisitors = length(unique(anonID)),newVisitors = sum(first_time))
+#-save table as CSV
+write.csv(by_day,"by_day.csv") #save table
+#-plot
+by_day.melt <- melt(by_day,"date") 
+ggplot(by_day.melt, aes(x=date,y=value)) + geom_line() + facet_grid(variable~.,scales="free") + 
+  ylab("") + xlab("date")
+ggsave("by_day.jpg")
+
+##----- total visitors, unique visitors, by day of week
+by_dow <- data %>%
   group_by(dow) %>%
-  summarise(avgVisitors = mean(n()), prcntNewVisitors = mean(first_time)) %>%
-  select(dow,avgVisitors,prcntNewVisitors) %>%
-  slice(c(2,6,7,5,1,3,4)) %>%
-  write.csv(.,"by_dow.csv")
+  summarise(avgVisitors = mean(n()), avgNewVisitors = mean(first_time)*n()) %>%
+  select(dow,avgVisitors,avgNewVisitors) %>%
+  slice(c(2,6,7,5,1,3,4)) 
+#-save table as CSV
+write.csv(by_dow,"by_dow.csv")
+#-plot
+by_dow.melt <- melt(by_dow,"dow") 
+ggplot(by_dow.melt, aes(x=dow,y=value,fill=variable)) + geom_bar(stat = "identity",position="dodge") + #facet_grid(variable~.,scales="free") + 
+  ylab("") + xlab("date")
+ggsave("by_dow")
 
 data %>%
   group_by(month) %>%
-  summarise(mean_n_visitors = mean(n()), percent_new_visitors = round(mean(first_time),2),date=mean(date)) %>%
+  summarise(avgVisitors = mean(n()), prcntNewVisitors = round(mean(first_time),2),date=mean(date)) %>%
   arrange(date) %>% 
   select(-date) %>%
   write.csv(.,"by_month.csv")
