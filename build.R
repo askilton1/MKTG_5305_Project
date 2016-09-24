e49fa823dc541744f2b4ed5c9a3a361483288bb1
askilton1 <antonio.skilton@gmail.com>
2016-09-24 12:56:07 -0700

    dow table now in right order, by_month.csv introduced

--- build.R
+++ build.R
@@ -2,12 +2,23 @@ source("clean.r")
 library(dplyr)
 data2 <- data %>%
   group_by(date) %>%
-  summarise(total_visitors = n(),unique_visitors = length(unique(anonID)),sum_new_visitors = sum(first_time)) %>%
-  write.csv(.,"by_day.csv")
-
-plot(data2$date,data2$sum_new_visitors)
+  summarise(total_visitors = n(),unique_visitors = length(unique(anonID)),sum_new_visitors = sum(first_time))
+write.csv(data2,"by_day.csv") 
+attach(data2)
+plot(date,sum_new_visitors,main = "New Visitors by Day \nBetween 6/30/14 and 7/1/15",ylab = "New Visitors")
+detach(data2);rm(data2)
 
 data %>%
   group_by(dow) %>%
-  summarise(mean_number_visitors = mean(n()), percent_new_visitors = mean(first_time)) %>%
+  summarise(avgVisitors = mean(n()), prcntNewVisitors = mean(first_time)) %>%
+  select(dow,avgVisitors,prcntNewVisitors) %>%
+  slice(c(2,6,7,5,1,3,4)) %>%
   write.csv(.,"by_dow.csv")
+
+data %>%
+  group_by(month) %>%
+  summarise(mean_n_visitors = mean(n()), percent_new_visitors = round(mean(first_time),2),date=mean(date)) %>%
+  arrange(date) %>% 
+  select(-date) %>%
+  write.csv(.,"by_month.csv")
+
