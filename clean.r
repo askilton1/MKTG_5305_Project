20b29ccf266fa9fc8b039b800339120a4d702d6d
askilton1 <antonio.skilton@gmail.com>
2016-10-03 22:54:17 -0700

    using abbreviated version of dow names

--- clean.r
+++ clean.r
@@ -5,9 +5,9 @@ data <- tbl_df(read.csv("data.csv"))
 print("cleaning data")
 data <- data %>% 
   mutate(date = as.POSIXct((Door.Access.Actual.DateTime),origin = "1960-01-01",format="%A, %B %d, %Y"),
-         dow = weekdays(date),
+         dow = weekdays(date,abbreviate=TRUE),
          month = months(date),
-         weekend = ifelse(dow=="Saturday" | dow=="Sunday",1,0),
+         weekend = ifelse(dow=="Sat" | dow=="Sun",1,0),
          class = Classification,
          turn1 = ifelse(Door.Name == "FITN-TRN1",1,0),
          anonID = as.factor(anonID)) %>%
