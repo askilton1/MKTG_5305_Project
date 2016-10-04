d17df3f79dc348fe74b74700bdc1f39eb1121c11
askilton1 <antonio.skilton@gmail.com>
2016-10-04 11:36:30 -0700

    turnstile choice as factor; keep door.access...... for calendar_fun()

--- clean.r
+++ clean.r
@@ -3,12 +3,11 @@ clean <- function(x){
   print("cleaning data")
   data <- tbl_df(x) %>% 
     mutate(class = Classification,
-           tunstile_1 = ifelse(Door.Name == "FITN-TRN1",1,0),
+           tunstile_1 = as.factor(ifelse(Door.Name == "FITN-TRN1",1,0)),
            anonID = as.factor(anonID)) %>%
-    select(-Door.Access.Actual.DateTime,-Result,-Door.Name,-Classification)
+    select(-Result,-Door.Name,-Classification)
 
   print("done")
   return(data)
 }
 
-x <- read.csv("data.csv")
