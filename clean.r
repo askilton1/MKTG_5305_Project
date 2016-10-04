9f02577241f84a094d1331105bd3820e07362fd9
askilton1 <antonio.skilton@gmail.com>
2016-10-04 10:12:58 -0700

    stripped clean() of all time related mutations

--- clean.r
+++ clean.r
@@ -2,15 +2,13 @@ clean <- function(x){
   suppressMessages(library(dplyr))
   print("cleaning data")
   data <- tbl_df(x) %>% 
-    mutate(date = as.POSIXct((Door.Access.Actual.DateTime),origin = "1960-01-01",format="%A, %B %d, %Y"),
-           class = Classification,
+    mutate(class = Classification,
            tunstile_1 = ifelse(Door.Name == "FITN-TRN1",1,0),
-           anonID = as.factor(anonID),
-           first_time = ifelse(1:length(anonID) %in% match(unique(anonID),anonID),1,0)) %>%
-    select(-Door.Access.Actual.DateTime,-Result,-Door.Name,-Classification) %>%
-    filter(date >= "2014-09-15" & date <= "2015-06-30")
+           anonID = as.factor(anonID)) %>%
+    select(-Door.Access.Actual.DateTime,-Result,-Door.Name,-Classification)
 
   print("done")
   return(data)
 }
 
+x <- read.csv("data.csv")
