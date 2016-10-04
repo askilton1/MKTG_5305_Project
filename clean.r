03ed9f1ac6c45abdf0c1324d4b8d4073d01442e6
askilton1 <antonio.skilton@gmail.com>
2016-10-04 10:06:14 -0700

    all mutations now within dplyr chain; output is now tibble; removed calendar mutations

--- clean.r
+++ clean.r
@@ -1,20 +1,16 @@
 clean <- function(x){
   suppressMessages(library(dplyr))
   print("cleaning data")
-  data <- x %>% 
+  data <- tbl_df(x) %>% 
     mutate(date = as.POSIXct((Door.Access.Actual.DateTime),origin = "1960-01-01",format="%A, %B %d, %Y"),
-           dow = weekdays(date,abbreviate=TRUE),
-           month = months(date),
-           weekend = ifelse(dow=="Sat" | dow=="Sun",1,0),
            class = Classification,
-           turn1 = ifelse(Door.Name == "FITN-TRN1",1,0),
-           anonID = as.factor(anonID)) %>%
+           tunstile_1 = ifelse(Door.Name == "FITN-TRN1",1,0),
+           anonID = as.factor(anonID),
+           first_time = ifelse(1:length(anonID) %in% match(unique(anonID),anonID),1,0)) %>%
     select(-Door.Access.Actual.DateTime,-Result,-Door.Name,-Classification) %>%
     filter(date >= "2014-09-15" & date <= "2015-06-30")
-  
-  data$first_time <- 0
-  data$first_time[match(unique(data$anonID),data$anonID)] <- 1
-  
+
   print("done")
   return(data)
-  }
+}
+
