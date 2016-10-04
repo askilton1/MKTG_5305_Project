8495397898be3264ebce63e568e4708372592da3
askilton1 <antonio.skilton@gmail.com>
2016-10-04 16:59:48 -0700

    removed commented lines

--- dateTime.R
+++ dateTime.R
@@ -6,12 +6,10 @@ calendar_fun <- function(x){
       year = as.factor(strftime(date,"%Y")),
       month = as.factor(months(date,abbreviate=TRUE)),
       dow = as.factor(weekdays(date,abbreviate=TRUE)),
-      #yearmonth = as.factor(paste(year,month,sep=" ")),
       week = as.numeric(format(date,"%W")),
       weekend = ifelse(dow=="Sat" | dow=="Sun",1,0)) %>%
     unite(yearmonth, year, month, sep="-", remove=FALSE) %>%
     group_by(yearmonth) %>% mutate(monthweek=1+week-min(week)) %>% ungroup() %>%
-    #mutate(monthweek=1+week-monthweek) %>%
     filter(date >= "2014-09-15" & date <= "2015-06-30") %>%
     select(-Door.Access.Actual.DateTime) %>%
   return()
