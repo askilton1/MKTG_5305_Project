320503b337d4fa178c94c5230fabeccbeecb190b
askilton1 <antonio.skilton@gmail.com>
2016-10-05 10:10:09 -0700

    now producing three heatmaps
    1) new_heatmap, 2) unique_heatmap, 3) all_minus_unique_heatmap

--- heatmap.R
+++ heatmap.R
@@ -2,11 +2,35 @@ library(ggplot2);library(dplyr)
 source("dateTime.R")
 source("AllUniqueNew.R")
 source("clean.r")
-read.csv("data.csv") %>%
+
+toPlot <-read.csv("data.csv") %>%
   clean() %>%
   calendar_fun() %>%
-  AllUniqueNew() %>%
-  ggplot(aes(monthweek, dow, fill = New)) + 
-    geom_tile(colour = "white") + facet_grid(~yearmonth) + scale_fill_gradient(low="yellow", high="red") +
-    ggtitle("Time-Series Calendar Heatmap") +  xlab("Week of Month") + ylab("") + theme(legend.position="bottom")
-ggsave("plots/heatmap.png")
+  AllUniqueNew()
+
+ggplot(toPlot,aes(monthweek, dow, fill = New)) + 
+    geom_tile(colour = "white") + 
+    facet_grid(~yearmonth,scales = "free") + 
+    scale_fill_gradient(low="yellow", high="red") +
+    ggtitle("Time-Series Calendar Heatmap") +  
+    xlab("Week of Month") + ylab("") + 
+    theme(legend.position="bottom")
+ggsave("plots/new_heatmap.png",width=10,height=3,units="in")
+
+ggplot(toPlot,aes(monthweek, dow, fill = Unique)) + 
+  geom_tile(colour = "white") + 
+  facet_grid(~yearmonth, scales = "free") + 
+  scale_fill_gradient(low="yellow", high = "red") +
+  ggtitle("Time-Series Calendar Heatmap") +  
+  xlab("Week of Month") + ylab("") + 
+  theme(legend.position="bottom")
+ggsave("plots/unique_heatmap.png",width=10,height=3,units="in")
+
+ggplot(toPlot,aes(monthweek, dow, fill = All-Unique)) + 
+  geom_tile(colour = "white") + 
+  facet_grid(~yearmonth, scales = "free") + 
+  scale_fill_gradient(low="yellow", high = "red") +
+  ggtitle("Time-Series Calendar Heatmap") +  
+  xlab("Week of Month") + ylab("") + 
+  theme(legend.position="bottom")
+ggsave("plots/all_minus_unique_heatmap.png",width=10,height=3,units="in")
