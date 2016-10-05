0acd0c9b3377e963c9b681edbb820989ca413ff7
askilton1 <antonio.skilton@gmail.com>
2016-10-04 17:40:31 -0700

    no more printing status

--- clean.r
+++ clean.r
@@ -1,13 +1,10 @@
 clean <- function(x){
   suppressMessages(library(dplyr))
-  print("cleaning data")
-  data <- tbl_df(x) %>% 
+  tbl_df(x) %>% 
     mutate(class = Classification,
            tunstile_1 = as.factor(ifelse(Door.Name == "FITN-TRN1",1,0)),
            anonID = as.factor(anonID)) %>%
-    select(-Result,-Door.Name,-Classification)
-
-  print("done")
-  return(data)
+    select(-Result,-Door.Name,-Classification) %>%
+  return()
 }
 
