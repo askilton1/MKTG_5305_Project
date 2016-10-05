ca322d75e7b1586c85ca3d1c4557dba6c5c0f9cd
askilton1 <antonio.skilton@gmail.com>
2016-10-04 18:22:13 -0700

    using functions drastically shortens code

--- heatmap.R
+++ heatmap.R
@@ -1,26 +1,12 @@
-require(ggplot2);require(reshape2);require(plyr)
-require(scales);library(dplyr);source("clean.r")
-
-data <- clean(read.csv("data.csv"))
-
-data <- tbl_df(data)
-data$year<-as.numeric(as.POSIXlt(data$date)$year+1900)
-# the month too 
-data$month<-as.numeric(as.POSIXlt(data$date)$mon+1)
-# data$weekday <- as.POSIXlt(data$date)$wday
-data$weekdayf <- factor(data$dow,levels=c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"),labels=rev(c("Mon","Tue","Wed","Thu","Fri","Sat","Sun")),ordered=TRUE)
-data$yearmonth<-as.yearmon(data$date)
-data$yearmonthf<-factor(data$yearmonth)
-# then find the "week of year" for each day
-data$week <- as.numeric(format(data$date,"%W"))
-# and now for each monthblock we normalize the week to start at 1 
-data<-ddply(data,.(yearmonthf),transform,monthweek=1+week-min(week))
-data <- data %>% mutate(monthweek = ifelse(monthweek==6,5,monthweek))
-data %>%
-  group_by(date) %>%
-  summarise(monthweek=unique(monthweek),weekdayf=unique(weekdayf),yearmonthf=unique(yearmonthf),
-    All = n(),Unique = length(unique(anonID)),New = sum(first_time)) %>%
-  ggplot(aes(monthweek, weekdayf, fill = New)) + 
-    geom_tile(colour = "white") + facet_grid(~yearmonthf) + scale_fill_gradient(low="yellow", high="red") +
+library(ggplot2);library(dplyr)
+source("dateTime.R")
+source("AllUniqueNew.R")
+source("clean.r")
+read.csv("data.csv") %>%
+  clean() %>%
+  calendar_fun() %>%
+  AllUniqueNew() %>%
+  ggplot(aes(monthweek, dow, fill = New)) + 
+    geom_tile(colour = "white") + facet_grid(~yearmonth) + scale_fill_gradient(low="yellow", high="red") +
     ggtitle("Time-Series Calendar Heatmap") +  xlab("Week of Month") + ylab("") + theme(legend.position="bottom")
 ggsave("plots/heatmap.png")
