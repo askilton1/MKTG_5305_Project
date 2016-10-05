984c83c2be76fc5eb44d8870ab01dde096e22f6e
askilton1 <antonio.skilton@gmail.com>
2016-10-04 17:18:45 -0700

    calender_fun() now filters for those who came to facility for the first
    )

--- dateTime.R
+++ dateTime.R
@@ -7,11 +7,12 @@ calendar_fun <- function(x){
       month = as.factor(months(date,abbreviate=TRUE)),
       dow = as.factor(weekdays(date,abbreviate=TRUE)),
       week = as.numeric(format(date,"%W")),
-      weekend = ifelse(dow=="Sat" | dow=="Sun",1,0)) %>%
+      weekend = ifelse(dow=="Sat" | dow=="Sun",1,0),
+      before_anonID = ifelse(date <= "2014-09-15",anonID,NA)) %>%
     unite(yearmonth, year, month, sep="-", remove=FALSE) %>%
     group_by(yearmonth) %>% mutate(monthweek=1+week-min(week)) %>% ungroup() %>%
-    filter(date >= "2014-09-15" & date <= "2015-06-30") %>%
-    select(-Door.Access.Actual.DateTime) %>%
+    filter(!is.na(before_anonID)) %>% 
+    #filter(date >= "2014-09-15" & date <= "2015-06-30") %>%
+    select(-Door.Access.Actual.DateTime,-before_anonID) %>%
   return()
 }
-
