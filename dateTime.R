c2f67ee9f10cedb9c549ab324401106a15e960c8
askilton1 <antonio.skilton@gmail.com>
2016-10-04 18:21:47 -0700

    month and year month factors now ordered

--- dateTime.R
+++ dateTime.R
@@ -1,18 +1,18 @@
-calendar_fun <- function(x){
+calendar_fun <- function(data){
   library(dplyr);library(tidyr)
   data %>%
     mutate(
       date = as.POSIXct((Door.Access.Actual.DateTime),origin = "1960-01-01",format="%A, %B %d, %Y"),
       year = as.factor(strftime(date,"%Y")),
-      month = as.factor(months(date,abbreviate=TRUE)),
+      month = factor(months(date,abbreviate=TRUE),levels=c("Sep","Oct","Nov","Dec","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug"),ordered=TRUE),
       dow = as.factor(weekdays(date,abbreviate=TRUE)),
       week = as.numeric(format(date,"%W")),
       weekend = ifelse(dow=="Sat" | dow=="Sun",1,0),
       before_anonID = ifelse(date >= "2014-09-15",1,0)) %>%
     unite(yearmonth, year, month, sep="-", remove=FALSE) %>%
     group_by(yearmonth) %>% mutate(monthweek=1+week-min(week)) %>% ungroup() %>%
-    filter(before_anonID==1) %>% 
-    #filter(date >= "2014-09-15" & date <= "2015-06-30") %>%
+    filter(before_anonID==1,date >= "2014-09-15" & date <= "2015-06-30") %>% 
+    mutate(yearmonth = factor(yearmonth,levels=c("2014-Sep","2014-Oct","2014-Nov","2014-Dec","2015-Jan","2015-Feb","2015-Mar","2015-Apr","2015-May","2015-Jun"))) %>%
     select(-Door.Access.Actual.DateTime,-before_anonID) %>%
   return()
 }
